365d6524c4f7a3ad553ca227f7a165a6ab3124f1
Toto Jati <totojati34@gmail.com>
2020-12-11 10:10:44 +0700

    menambah fungsi landing element

--- script.js
+++ script.js
@@ -12,3 +12,55 @@ $(".nav-link").click(function(e){
 
     e.preventDefault();
 });
+
+$(window).scroll(function(){
+    let myScroll = $(this).scrollTop();
+    
+    if (myScroll > $("#services").offset().top - 150){
+        $(".service").each(function(i){
+            setTimeout(function(){
+                $(".service").eq(i).css({
+                    "transform": "translateY(0)",
+                    "opacity": "1"
+                });
+            }, (400 * i));
+        });
+    }
+    if (myScroll > $("#items1").offset().top - 50){
+        $("#items1 .card").each(function(i){
+            setTimeout(function(){
+                $("#items1 .card").eq(i).css({
+                    "transform": "translateY(0)",
+                    "opacity": "1"
+                });
+            }, (400 * i));
+        });
+    }
+    if (myScroll > $("#items2").offset().top - 200){
+        $("#items2 .card").each(function(i){
+            setTimeout(function(){
+                $("#items2 .card").eq(i).css({
+                    "transform": "translateY(0)",
+                    "opacity": "1"
+                });
+            }, (400 * i));
+        });
+    }
+
+    if (myScroll > $("#article1").offset().top - 200){
+        $("#article1 .right-article, #article1 .left-article").css({
+            "transform": "translateX(0)",
+            "opacity": "1"
+        });
+    }
+    if (myScroll > $("#article2").offset().top - 200){
+        $("#article2 .right-article, #article2 .left-article").css({
+            "transform": "translateX(0)",
+            "opacity": "1"
+        });
+    }
+    if (myScroll > $("#sponsor").offset().top - 350){
+        $("#sponsor").css("opacity", "1");
+    }
+    
+})
